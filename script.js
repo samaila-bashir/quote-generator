192199513686d93ef82486ff6f87b8c546cbd85d
Samaila Chatto Bashir <chattobashir@gmail.com>
2022-01-18 11:15:20 +0100

    Implemented dynamic quotes and authors from API

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show New Quote
@@ -5,7 +11,8 @@ function newQuote()
 {
     // Pick a random quote from apiQuote array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote)
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
