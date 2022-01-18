e751d2319fb1ce7527fce4af810a01640363b8b0
Samaila Chatto Bashir <chattobashir@gmail.com>
2022-01-18 11:20:09 +0100

    Check for empty authors and apply default values

--- script.js
+++ script.js
@@ -11,8 +11,15 @@ function newQuote()
 {
     // Pick a random quote from apiQuote array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    authorText.textContent = quote.author;
     quoteText.textContent = quote.text;
+
+    if (!quote.author)
+    {
+        authorText.textContent = 'Unknown';
+    } else
+    {
+        authorText.textContent = quote.author;
+    }
 }
 
 // Get Quotes From API
