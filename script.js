bfeef6ecde370ba7e893e6f3727b3fc566939771
Samaila Chatto Bashir <chattobashir@gmail.com>
2022-01-18 11:53:29 +0100

    Added a loader before quote displays

--- script.js
+++ script.js
@@ -3,12 +3,28 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
+
+// Show loading
+function loading()
+{
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete()
+{
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
 
 // Show New Quote
 function newQuote()
 {
+    loading();
     // Pick a random quote from apiQuote array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -20,7 +36,9 @@ function newQuote()
         quoteText.classList.remove('long-quote');
     }
 
+    // Set quote and hide loader
     quoteText.textContent = quote.text;
+    complete();
 
     if (!quote.author)
     {
@@ -34,6 +52,7 @@ function newQuote()
 // Get Quotes From API
 async function getQuotes()
 {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
 
     try {
