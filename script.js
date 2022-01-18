684032c1767bffa301bcf26b40d4e0e881329c45
Samaila Chatto Bashir <chattobashir@gmail.com>
2022-01-18 11:04:12 +0100

    Implemented fetch request for API quotes

---
+++ script.js
@@ -0,0 +1,26 @@
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote()
+{
+    // Pick a random quote from apiQuote array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote)
+}
+
+// Get Quotes From API
+async function getQuotes()
+{
+    const apiUrl = 'https://type.fit/api/quotes';
+
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getQuotes();
